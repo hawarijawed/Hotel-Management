58393fba7104c8894823caac8ce25afb1da63a8f
hawarijawed <hawarijawed143@gmail.com>
2025-07-17 18:44:55 +0530

    Client folder added to main project

---
+++ Client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return (
+    <>
+      <p>My name is Jawed Hawari</p>
+      <p>Aur batao Kaise Ho</p>
+    </>
+  )
+}
+
+export default App
