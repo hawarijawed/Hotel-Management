d7c1f2f96147523b3cd492d22318552b7a57c6b1
hawarijawed <hawarijawed143@gmail.com>
2025-08-01 19:11:07 +0545

    Addin Room Details page

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -4,6 +4,7 @@ import { Routes,Route, useLocation } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
 import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
       <Routes>
         <Route path='/'  element={<Home />}/>
         <Route path='/rooms' element={<AllRooms />}/>
+        <Route path='/rooms/:id' element={<RoomDetails />} />
       </Routes>
     </div>
     <Footer />
---
+++ Client/src/pages/RoomDetails.jsx
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { assets, roomsDummyData } from '../assets/assets';
+import StarRating from '../components/StarRating';
+
+const RoomDetails = () => {
+    const {id} = useParams();
+    const [room, setRoom] = useState(null);
+    const [mainImage, SetMainImage] = useState(null);
+    
+    useEffect(()=>{
+        const room = roomsDummyData.find(room => room._id === id);
+        room && setRoom(room)
+        room && SetMainImage(room.images[0]);
+
+    }, []);
+    return room &&  (
+    <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
+        {/* Room Details */}
+        <div className='flex flex-col md:flex-row items-start md:items-center gap-2'>
+            <h1 className='text-3xl md:text-4xl font-playfair'>{room.hotel.name} <span className='font-inner text-sm'>({room.roomType})</span></h1>
+            <p className='text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full'>20% OFF</p>
+        </div>
+
+        {/* Room Rating */}
+        <div className='flex items-center gap-1 mt-2'>
+            <StarRating />
+            <p className='ml-2'>200+ Reviews</p>
+        </div>
+
+        {/* Room Address */}
+        <div className='flex items-center gap-1 text-gray-500 mt-2'>
+            <img src={assets.locationIcon} alt="location-img" />
+            <span>{room.hotel.address}</span>
+        </div>
+
+        {/* Room Images */}
+        <div className='flex flex-col lg:flex-row mt-6 gap-6'>
+            <div className='lg:w-1/2 w-full'>
+                <img src={mainImage} alt="mainImage" className='w-full rounded-xl shadow-lg object-cover'/>
+            </div>
+            <div className='grid grid-cols-2 gap-3 lg:w-1/2 w-full '>
+                {room?.images.length > 1 &&room.images.map((image, index)=>(
+                    <img onClick={()=>SetMainImage(image)}
+                    src={image} alt="room img" key={index}
+                    className={`w-full rounded-xl shadow-md object-cover cursor-pointer ${mainImage === image && 'outline-3 outline-orange-500'}`} />
+                ))}
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default RoomDetails
