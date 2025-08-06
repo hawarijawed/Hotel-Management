407d1e2dfdf065a768834904983240a408aa5142
hawarijawed <hawarijawed143@gmail.com>
2025-08-06 16:23:34 +0545

    Adding owner pages

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -7,6 +7,7 @@ import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
+import Layout from './pages/HotelOwner/Layout';
 
 
 function App() {
@@ -24,7 +25,9 @@ function App() {
         <Route path='/rooms' element={<AllRooms />}/>
         <Route path='/rooms/:id' element={<RoomDetails />} />
         <Route path='/my-bookings' element={<MyBookings />}/>
-       
+        <Route path='/owner' element={<Layout />}>
+
+        </Route>
       </Routes>
     </div>
     <Footer />
