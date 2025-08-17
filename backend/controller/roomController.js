aa6d1e5293d7f2220d5ea13bd86b589adbd3b066
hawarijawed <hawarijawed143@gmail.com>
2025-08-17 19:59:11 +0545

    Completing the routing for room data

--- backend/controller/roomController.js
+++ backend/controller/roomController.js
@@ -36,15 +36,43 @@ export const createRoom = async (req, res) => {
 
 //Getting all the rooms
 export const getRooms = async (req, res) => {
-
+    try {
+        const rooms = await Room.find({isAvailable:true}).populate({
+            path:'hotel',
+            populate:{
+                path:'owner',
+                select:'image'
+            }
+        }).sort({createdAt:-1});
+        res.json({success:true, rooms});
+    } catch (error) {
+        res.json({success:false, message:error.message});
+    }
 }
 
 //Hotel specific rooms
 export const getOwnerRooms = async(req, res) => {
-
+    try {
+        const hotelData = await Hotel({owner:req.auth.userId});
+        const rooms = await Room.find({hotel:hotelData._id.toString()}).populate("hotel");
+        res.json({success:true, rooms});
+    } catch (error) {
+        res.json({success:false, message:error.message});
+    }
+    
 }
 
 //API to change the availability of the room
 export const toggleRoomAvailability = async(req, res) => {
 
+    try {
+        const {roomId} = req.body;
+        const roomData = await Room.findById(roomId);
+        roomData.isAvailable = !roomData.isAvailable;
+        await roomData.save();
+
+        res.json({success:true, message:"Room availability updated successfully"})
+    } catch (error) {
+        res.json({success:false, message:error.message});
+    }
 }
