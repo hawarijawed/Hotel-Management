5c35051ad9da3e3185238bba8fa17476d8e03d17
hawarijawed <hawarijawed143@gmail.com>
2025-08-17 12:53:32 +0545

    Handling user route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './config/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import clerkWebHooks from './controller/clerkWebhooks.js';
+import userRouter from './routes/userRoutes.js';
 
 // await connectDB();
 const PORT = process.env.PORT || 8000;
@@ -21,4 +22,6 @@ app.use(express.json());
 
 app.get('/',(req, res) =>res.send("Hello from server..we are do date"));
 
+
+app.use('/api/user', userRouter);
 app.listen(PORT,()=>console.log(`App started...: http://localhost:${PORT}/`))
