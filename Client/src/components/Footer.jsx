7d604e827aae53c5e630fe8a8beb4f2f2cafabd2
hawarijawed <hawarijawed143@gmail.com>
2025-08-23 14:45:54 +0545

    Troubleshooting the bugs

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { assets } from '../assets/assets'
-import favicon from '../../public/favicon.svg'
+import { assets } from '../assets/assets.js'
+import favicon from '/favicon.svg?url';
 const Footer = () => {
   return (
     <div className='bg-[#F6F9FC] text-gray-500/80 pt-8 px-6 md:px-16 lg:px-24 xl:px-32'>
